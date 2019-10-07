697858588b3f4e7dbe61743e07743f6cb7f709ba
Alex Ngari <alex.ngari03@gmail.com>
2019-10-07 20:56:09 +0300

    Fixed purchasePackage's connection to datastore

--- client-app/1-triggerPubSub.js
+++ client-app/1-triggerPubSub.js
@@ -1,6 +1,6 @@
 const path = require('path');
 const chalk = require('chalk');
-const PubSub = require('@google-cloud/pubsub');
+const { PubSub } = require('@google-cloud/pubsub');
 const testData = require('./testData.json');
 
 const projectId = process.env.GCLOUD_PROJECT_ID;
@@ -8,18 +8,18 @@ const pubsub = new PubSub({
     projectId: projectId
 });
 
-const topic = pubsub.topic(process.env.TOPIC_NAME);
+// const topic = pubsub.topic(process.env.TOPIC_NAME);
 
 const maxMessages = 100;
 const maxMilliseconds = 10000;
-const publisher = topic.publisher({
+const publisher = pubsub.topic(process.env.TOPIC_NAME, {
     batching: {
-      maxMessages: maxMessages,
-      maxMilliseconds: maxMilliseconds,
+        maxMessages: maxMessages,
+        maxMilliseconds: maxMilliseconds,
     },
-  });
+});
 
-const invocations = [5, 4, 3, 2, 1].map(x => x * 10);
+const invocations = [5, 4, 3, 2, 1].map(x => x * 3);
 
 let promises = [];
 
@@ -27,13 +27,13 @@ for (data of testData) {
     for (let index = 0; index < invocations.length; index++) {
         const totalInvocations = invocations[index];
         const package = data.packages[index];
-    
+
         const pubData = {
             projectId: data.projectId,
             packageId: package.id,
             value: package.value
         }
-    
+
         const dataBuffer = Buffer.from(JSON.stringify(pubData));
         for (let invocation = 0; invocation < totalInvocations; invocation++) {
             const promise = publisher.publish(dataBuffer).then(message => {
